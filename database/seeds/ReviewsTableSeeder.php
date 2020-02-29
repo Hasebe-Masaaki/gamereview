6753e8fd19e79cf787abaf74cb0e45ebe28885f1
Hasebe-Masaaki <hasebe516@kir.biglobe.ne.jp>
2020-03-01 00:52:06 +0900

    seeder commit

--- database/seeds/ReviewsTableSeeder.php
+++ database/seeds/ReviewsTableSeeder.php
@@ -19,7 +19,7 @@ class ReviewsTableSeeder extends CsvSeeder
 
     // SET auto-increment start value
     if (config('database.default') == 'mysql') {
-        DB::statement("ALTER TABLE users AUTO_INCREMENT = 10001;");
+        DB::statement("ALTER TABLE reviews AUTO_INCREMENT = 10001;");
     }
     elseif (config('database.default') == 'pgsql') {
         DB::statement("select setval ('reviews_id_seq', 10000);");
